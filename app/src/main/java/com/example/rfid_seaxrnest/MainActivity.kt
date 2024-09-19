b0a6ec1d357163d957a36d7e74b186ce713c26c2
FaradayB <faradaybarrf@gmail.com>
2024-09-19 08:35:24 +0700

    Changed MainActivity to HomeActivity and added LoginActivity

--- app/src/main/java/com/example/rfid_seaxrnest/MainActivity.kt
+++ app/src/main/java/com/example/rfid_seaxrnest/MainActivity.kt
@@ -10,43 +10,5 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        // Enable edge-to-edge mode
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-
-        setContentView(R.layout.activity_main)
-
-        // Handle window insets for edge-to-edge content
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        // Set up Bottom Navigation
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_home -> switchFragment(HomeFragment())
-                R.id.nav_rooms -> switchFragment(RoomsFragment())
-                R.id.nav_items -> switchFragment(ItemsFragment())
-                R.id.nav_profile -> switchFragment(ProfileFragment())
-            }
-            true
-        }
-
-        // Load the default fragment (Home) when the activity is created
-        if (savedInstanceState == null) {
-            switchFragment(HomeFragment())
-        }
-    }
-
-    private fun switchFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .commit()
-    }
 }
