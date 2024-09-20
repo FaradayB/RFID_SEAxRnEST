7dad360688a91484cc28c42ee86e73e56246a44b
FaradayB <faradaybarrf@gmail.com>
2024-09-20 13:11:52 +0700

    Done with login and all the login properties

--- app/src/main/java/com/example/rfid_seaxrnest/LoginActivity.kt
+++ app/src/main/java/com/example/rfid_seaxrnest/LoginActivity.kt
@@ -1,6 +1,11 @@
 package com.example.rfid_seaxrnest
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -14,6 +19,30 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         auth = Firebase.auth
+
+        val email = findViewById<EditText>(R.id.et_email)
+        val password = findViewById<EditText>(R.id.et_pass)
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+
+        btnLogin.setOnClickListener {
+            val emailInput = email.text.toString()
+            val passInput = password.text.toString()
+
+            auth.signInWithEmailAndPassword(emailInput, passInput)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d("LoginActivity", "Email is: $emailInput")
+                        Log.d("LoginActivity", "Password is: $passInput")
+
+                        val user = auth.currentUser
+                        val intent = Intent(this, HomeActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else {
+                        Toast.makeText(baseContext, "Authentication failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
 
     }
 }
