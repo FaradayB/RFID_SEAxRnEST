8589946eab4693d925934514355032675b65121c
FaradayB <faradaybarrf@gmail.com>
2024-09-20 11:06:56 +0700

    done designing main activity

--- app/src/main/java/com/example/rfid_seaxrnest/MainActivity.kt
+++ app/src/main/java/com/example/rfid_seaxrnest/MainActivity.kt
@@ -2,15 +2,8 @@ package com.example.rfid_seaxrnest
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.CalendarContract.Instances
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
